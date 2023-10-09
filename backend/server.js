894c738059bff23cecf2936212da20da2dd3fe13
Nahuel Borghi <69876615+NahuelBorghi@users.noreply.github.com>
2023-10-09 09:08:30 -0300

    Update server.js para levantar front
    ahora levantamos el front desde aca para evitar usar live server. live server recarga la pagina cada vez que filesystem guarda el archivo json

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
-const fs = require("fs");
+const fs = require("fs").promises; // Importar fs.promises para usar promesas
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-let chatMessages = [];
-getMessages();
 
 const PORT = 3000;
 
-// Configurar las rutas y middleware de Express aquí (si es necesario)
+app.use(express.static("../"));
 
 server.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
@@ -20,69 +18,47 @@ server.listen(PORT, () => {
 // WebSocket para mensajes
 wss.on("connection", (ws) => {
   console.log("Conexión para mensajes: OK");
-  ws.on("message", (message) => {
+  ws.on("message", async (message) => {
     try {
       const data = JSON.parse(message);
       if (data.state) {
         // Hacer algo específico para el estado recibido
         console.log(`${data.username} recibido en estado: ${data.state}`);
-        ws.send(`${message.toString()}`);
+        ws.send(message.toString());
       } else {
         console.log(`${data}`);
-        chatMessages.push(data);
-        // Guardar los mensajes en el archivo JSON
-        fs.writeFile(
-          "chat_messages.json",
-          JSON.stringify(chatMessages),
-          (err) => {
-            if (err) {
-              console.error(
-                "Error al guardar los mensajes en el archivo JSON:",
-                err
-              );
-            } else {
-              console.log("Mensaje guardado en el archivo JSON.");
-            }
-          }
-        );
+        // Guardar los mensajes en el archivo de texto
+        await saveMessage(message);
         console.log(`Mensaje recibido del frontend: ${message}`);
         // Broadcast del mensaje a todos los clientes (WebSocket)
-        wss.clients.forEach((client) => {
-          if (client !== ws && client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify(messageData));
-          }
-        });
+        ws.send(message.toString());
       }
     } catch (error) {
       console.error("Error al analizar el mensaje JSON:", error);
     }
   });
 });
-function getMessages() {
-  fs.readFile("chat_messages.json", "utf8", (err, data) => {
-    if (!err) {
-      try {
-        chatMessages = JSON.parse(data);
-        console.log("Mensajes cargados desde el archivo JSON.");
-      } catch (err) {
-        console.error("Error al analizar el archivo JSON de mensajes:", err);
-      }
-    }
-  });
-}
-// WebSocket para usuarios y estados
-const userWebSocket = new WebSocket.Server({ noServer: true });
 
-userWebSocket.on("connection", (ws) => {
-  console.log("conexión de usuarios y estados: OK");
-
-  // Aquí puedes manejar la información de usuarios y estados
-});
-
-server.on("upgrade", (request, socket, head) => {
-  if (request.url === "/user-socket") {
-    userWebSocket.handleUpgrade(request, socket, head, (ws) => {
-      userWebSocket.emit("connection", ws, request);
-    });
+async function getMessages() {
+  try {
+    const data = await fs.readFile("chat_messages.txt", "utf8");
+    console.log("Mensajes cargados desde el archivo de texto.");
+    return data.split("\n").filter((message) => message.trim() !== "");
+  } catch (err) {
+    console.error("Error al leer el archivo de texto de mensajes:", err);
+    return [];
   }
-});
+}
+
+async function saveMessage(message) {
+  try {
+    const chatMessages = await getMessages();
+    chatMessages.push(message); // Agrega el nuevo mensaje al arreglo de mensajes
+    const textData = chatMessages.join("\n");
+
+    await fs.writeFile("chat_messages.txt", textData);
+    console.log("Mensaje guardado en el archivo de texto.");
+  } catch (err) {
+    console.error("Error al guardar los mensajes en el archivo de texto:", err);
+  }
+}
