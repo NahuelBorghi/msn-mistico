c5c85435d47626638c81cfd3272847c272146989
NahuelBorghi <nahuelborghi@gmail.com>
2023-10-10 18:39:15 -0300

    sistema de desconexion y mensajes entre dispositivos

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,32 +1,54 @@
-const socket = new WebSocket("ws://localhost:3000");
+const socket = new WebSocket("ws://192.168.100.13:3000");
 const user = sessionStorage.getItem("username");
 const state = sessionStorage.getItem("state");
+
+if (!user) {
+  window.location.href = "/index.html";
+} else {
+  document.getElementById("user").innerHTML = user;
+}
 
 socket.addEventListener("open", () => {
   console.log("Conexión WebSocket establecida con el servidor");
   socket.send(JSON.stringify({ username: user, state: state }));
+  // Cargar los mensajes desde el Local Storage
+  const savedMessages = loadMessages();
+
+  // Obtener el contenedor de chat por su ID
+  const chatContainer = document.getElementById("chat-container");
+
+  // Crear elementos HTML para los mensajes y agregarlos al contenedor
+  savedMessages.forEach((message) => {
+    const nuevaPlantilla = createMessageTemplate(message);
+    if (message.user === user) {
+      nuevaPlantilla.classList.add("own-message");
+    }
+    chatContainer.appendChild(nuevaPlantilla);
+  });
 });
+
 socket.addEventListener("message", (event) => {
   console.log(event.data);
   const datos = JSON.parse(event.data);
-
-  // Obtener el nombre de usuario del sessionStorage
-  const currentUser = sessionStorage.getItem("username");
   if (datos.message) {
     const nuevaPlantilla = createMessageTemplate(datos);
     const contenedor = document.getElementById("chat-container");
     // Marcar mensajes propios como "own-message"
-    if (datos.user === currentUser) {
+    if (datos.user === user) {
       nuevaPlantilla.classList.add("own-message");
     }
     contenedor.appendChild(nuevaPlantilla);
+
+    // Actualizar y guardar los mensajes en el Local Storage
+    const chatMessages = loadMessages();
+    chatMessages.push(datos);
+    saveMessages(chatMessages);
   } else {
     handleFriendState(datos);
   }
 });
 
 ///event listeners
-
 const categoryHeaders = document.querySelectorAll("#contact-list h3");
 categoryHeaders.forEach((header) => {
   header.addEventListener("click", toggleCategoryList);
@@ -41,9 +63,15 @@ document.getElementById("message").addEventListener("keypress", (event) => {
     sendMessage();
   }
 });
+document.getElementById("logOut").addEventListener("click", logOut);
+window.addEventListener("beforeunload", (event) => {
+  // Realiza una solicitud WebSocket para notificar al servidor que el usuario se desconecta antes de cerrar la ventana
+  socket.send(JSON.stringify({ type: "disconnect" }));
+  // Puedes agregar un mensaje personalizado para confirmar la desconexión si es necesario
+  event.returnValue = "¿Seguro que deseas cerrar la ventana?";
+});
 
 /// Chat funtions
-
 function sendMessage() {
   const message = document.getElementById("message").value;
   if (message.trim() !== "") {
@@ -54,16 +82,59 @@ function sendMessage() {
       date: new Date().toISOString(),
     };
     socket.send(JSON.stringify(data));
-    console.log("aca no la cague");
   }
 }
+function saveMessages() {
+  // Obtener el contenedor de chat por su ID
+  const chatContainer = document.getElementById("chat-container");
+  // Crear un array para almacenar los mensajes
+  const messages = [];
+  // Obtener todos los elementos hijos del contenedor
+  const messageElements = chatContainer.children;
+  // Recorrer los elementos y extraer la información
+  for (let i = 0; i < messageElements.length; i++) {
+    const messageElement = messageElements[i];
+    // Extraer la información del mensaje
+    const userElement = messageElement.querySelector(".user");
+    // Eliminar " dice:" del nombre de usuario
+    const userText = userElement.textContent.replace(" dice:", "").trim();
+    const messageText =
+      messageElement.querySelector(".message-text").textContent;
+    const dateText = messageElement.querySelector(".date").textContent;
+    // Crear un objeto con la información y agregarlo al array
+    const messageObject = {
+      user: userText,
+      message: messageText,
+      date: dateText,
+    };
+    messages.push(messageObject);
+  }
+  // Convertir el array de objetos a una cadena JSON
+  const messagesJson = JSON.stringify(messages);
+  // Guardar la cadena JSON en el Local Storage
+  localStorage.setItem("chatMessages", messagesJson);
+}
 function createMessageTemplate(data) {
+  const fecha = new Date(data.date);
+  const fechaFormateada = `${fecha.getDate()}/${
+    fecha.getMonth() + 1
+  }/${fecha.getFullYear()} ${fecha.getHours()}:${fecha.getMinutes()}`;
   const nuevaPlantilla = document.createElement("div");
+  nuevaPlantilla.classList.add("message");
   nuevaPlantilla.innerHTML = `
-  <p>${data.user} dice:</p>
-  <p>${data.message}</p>
-  `;
+    <p class="user">${data.user} dice:</p>
+    <p class="message-text">${data.message}</p>
+    <p class="date">${fechaFormateada}</p>
+    `;
   return nuevaPlantilla;
+}
+function loadMessages() {
+  const messagesJson = localStorage.getItem("chatMessages");
+  if (messagesJson) {
+    return JSON.parse(messagesJson);
+  } else {
+    return [];
+  }
 }
 
 //friends list
@@ -80,11 +151,20 @@ function toggleCategoryList() {
   }
 }
 function handleFriendState(datos) {
-  const friendElement = createFriendStateElement(datos);
-  const userList = getUserListByState(datos.state);
-  if (userList) {
-    userList.appendChild(friendElement);
-  }
+  // Limpia todas las listas de usuarios
+  const onlineUserList = document.getElementById("connected-list");
+  const offlineUserList = document.getElementById("disconnected-list");
+  onlineUserList.innerHTML = "";
+  offlineUserList.innerHTML = "";
+  datos.forEach((user) => {
+    const friendElement = createFriendStateElement(user);
+
+    if (user.state === "online" || user.state === "busy") {
+      onlineUserList.appendChild(friendElement);
+    } else if (user.state === "offline") {
+      offlineUserList.appendChild(friendElement);
+    }
+  });
 }
 function createFriendStateElement(datos) {
   const friendElement = document.createElement("li");
@@ -101,4 +181,11 @@ function getUserListByState(state) {
   } else {
     return document.getElementById("connected-list");
   }
+}
+
+//log out
+function logOut() {
+  sessionStorage.removeItem("username");
+  sessionStorage.removeItem("state");
+  window.location.href = "/index.html";
 }
